4f12533177b49915698b38cb31fe73f165a09a51
ALLABER <allaberdinx@gmail.com>
2022-12-04 10:33:53 +0600

    add app icon and splash screen

--- app/src/main/java/ru/ufanet/design/MainActivity.kt
+++ app/src/main/java/ru/ufanet/design/MainActivity.kt
@@ -2,10 +2,30 @@ package ru.ufanet.design
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import ru.ufanet.design.databinding.ActivityMainBinding
+import ru.ufanet.ui_kit.customView.test.EmotionalFaceView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.emotionalFaceView.setOnClickListener{
+            setState()
+        }
+    }
+
+    private fun setState() {
+        if (binding.emotionalFaceView.happinessState == EmotionalFaceView.SAD) {
+            binding.emotionalFaceView.happinessState = EmotionalFaceView.HAPPY
+        } else {
+            binding.emotionalFaceView.happinessState = EmotionalFaceView.SAD
+        }
     }
 }
